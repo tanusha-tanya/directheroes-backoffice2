3ef571903a6fc42aa2686842c392e27bed123a17
Anton Dorofeev <anton@dorofeev.me>
2018-08-01 17:08:49 +0200

    new sf 4.1 project with encore & vuejs

---
+++ src/Controller/MainController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class MainController extends Controller
+{
+    /**
+     * @Route("/", name="main")
+     */
+    public function index()
+    {
+        return $this->render('base.html.twig');
+
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/MainController.php',
+        ]);
+    }
+}
