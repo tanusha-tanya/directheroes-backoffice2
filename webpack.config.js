3e934fa8ebf3060ae91afab991c7c4b3d11e9f02
Anton Dorofeev <anton@dorofeev.me>
2019-02-01 21:09:34 +0200

    [feature] atomic encore build

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 Encore
     // directory where compiled assets will be stored
-    .setOutputPath('public/build/')
+    .setOutputPath('public/build.tmp/')
     // public path used by the web server to access the output path
     .setPublicPath(process.env.PUBLIC_SCHEME + '://' + process.env.VIRTUAL_HOST + '/build/')
     // only needed for CDN's or sub-directory deploy
