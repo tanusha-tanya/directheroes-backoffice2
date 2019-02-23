344b9859644a23454a75e7e65d306906cff34a58
Andrey Matukhnov <dronic.x@gmail.com>
2019-02-24 00:06:36 +0200

    Add subscription dialog

--- assets/js/src/routes/accountDHSubscription.vue
+++ assets/js/src/routes/accountDHSubscription.vue
@@ -4,62 +4,73 @@
     <div class="account-dh-subscription-controls">
       <button>+ Add new account</button>
     </div>
-      <div class="list-item header">
-        <div class="account-row">Account</div>
-        <div class="subscriprion-row">Subscription</div>
-        <div class="followers-row">Followers</div>
-        <div class="renewalat-row">Renewal at</div>
-      </div>
-      <template v-if="subscriptions">
-        <template v-if="subscriptions.length">
-          <div class="list-item" v-for="subscription in subscriptions" :key="subscription.id" @click="accountToView = subscription">
-            <div class="account-row">
-              <div class="account-avatar" :style="{'background-image': `url(${ subscription.profilePicUrl })`}"></div>
-              <div>
-                {{ subscription.fullName }}
-                <div class="account-login">
-                  {{ subscription.login }}
-                </div>
+    <div class="list-item header">
+      <div class="account-row">Account</div>
+      <div class="subscriprion-row">Subscription</div>
+      <div class="followers-row">Followers</div>
+      <div class="renewalat-row">Renewal at</div>
+    </div>
+    <template v-if="subscriptions">
+      <template v-if="subscriptions.length">
+        <div class="list-item" v-for="subscription in subscriptions" :key="subscription.id" @click="accountToView = subscription">
+          <div class="account-row">
+            <div class="account-avatar" :style="{'background-image': `url(${ subscription.profilePicUrl })`}"></div>
+            <div>
+              {{ subscription.fullName }}
+              <div class="account-login">
+                {{ subscription.login }}
               </div>
             </div>
-            <div class="subscriprion-row">
-              <div :class="[{'subscription-type': true}, subscription.type]">
-                {{ subscription.type || 'FREE' }}
-              </div>
-            </div>
-            <div class="followers-row">{{ subscription.followerCount || 0 }}</div>
-            <div class="renewalat-row">Renewal at</div>
           </div>
-          <div class="list-item" v-for="thread in threads" :key="thread.id">
-            <div class="user-row">
-              <div class="user-avatar" :style="{'background-image': `url(${ thread.contactProfile.profilePicUrl })`}"></div>
-              {{thread.contactProfile.username}}
-            </div>
-            <div class="subscribed-row">{{ thread.subscribedAt ? subscriberAt(thread.subscribedAt) : ''}}</div>
-            <div class="lastmessage-row">{{subscriberAt(thread.lastMessageAt)}}</div>
-            <div class="campaigns-row">
-              <router-link
-                :to="{ name: 'accountCampaign', params: { campaignId: campaign.id } }"
-                v-for="campaign in thread.campaignList"
-                :key="campaign.id"
-                >{{campaign.name}}</router-link>
-            </div>
-            <div class="chat-row">
-              <router-link :to="{ name: 'accountThreadMessages', params: { threadId: thread.id, subscribed: subscribedText } }">Live chat</router-link>
-              <router-link :to="{ name: 'accountThreadInfo', params: { subscriberId: thread.id } }" class="account-button" ><img :src="avatar"/></router-link>
+          <div class="subscriprion-row">
+            <div :class="[{'subscription-type': true}, subscription.type]">
+              {{ subscription.type || 'FREE' }}
             </div>
           </div>
-        </template>
-        <div class="no-result" v-else>
-          No subscription found
+          <div class="followers-row">{{ subscription.followerCount || 0 }}</div>
+          <div class="renewalat-row">Renewal at</div>
         </div>
       </template>
-      <div class="loading-content" v-else>
-        <div class="pre-loader"></div>
+      <div class="no-result" v-else>
+        No subscription found
       </div>
+    </template>
+    <div class="loading-content" v-else>
+      <div class="pre-loader"></div>
+    </div>
+    <el-dialog
+      :visible.sync="isAccountToView"
+      width="207px"
+      append-to-body
+      class="subscription-dialog"
+      :show-close="false"
+      >
+        <div class="account-dh-info" v-if="accountToView">
+          <div class="account-dh-photo" :style="{'background-image': `url(${ accountToView.profilePicUrl || igAvatar })`}"></div>
+          <div class="account-dh-name">
+            {{accountToView.fullName}}
+          </div>
+          <div class="account-dh-login">
+            {{accountToView.login}}
+          </div>
+          <div class="account-plan">
+            <span>Subscription plan</span>
+            <div :class="[{'subscription-type': true}, accountToView.type]">
+              {{ accountToView.type || 'FREE' }}
+            </div>
+          </div>
+          <div class="account-dh-joined">
+            Joined: <strong>Aug 1st, 2018</strong>
+          </div>
+          <button>Cancel account</button>
+          <button>Delete account</button>
+        </div>
+    </el-dialog>
   </div>
 </template>
 <script>
+import igAvatar from '../assets/ig-avatar.jpg'
+
 export default {
   data() {
     return {
@@ -71,7 +82,7 @@ export default {
   computed: {
     isAccountToView:{
       get() {
-        return this.accountToView
+        return Boolean(this.accountToView)
       },
       set(value) {
         this.accountToView = value || null;
@@ -148,15 +159,6 @@ export default {
       width: 20%;
     }
 
-    .subscription-type {
-      display: inline-block;
-      font-weight: bold;
-      text-transform: uppercase;
-      color: #fff;
-      background-color: #A4A4A4;
-      border-radius: 5px;
-      padding: 5px 10px;
-    }
   }
 
   button {
@@ -165,6 +167,69 @@ export default {
     line-height: 16px;
     font-size: 16px;
     padding: 8px 25px;
+  }
+}
+
+.subscription-type {
+  display: inline-block;
+  font-weight: bold;
+  text-transform: uppercase;
+  color: #fff;
+  background-color: #A4A4A4;
+  border-radius: 5px;
+  padding: 5px 10px;
+}
+
+.subscription-dialog{
+  .el-dialog {
+    border-radius: 10px;
+  }
+
+  .el-dialog__header {
+    display: none;
+  }
+
+  .el-dialog__body {
+    padding: 0;
+  }
+
+  .account-dh-info {
+    width: 100%;
+    border-right: none;
+    margin-right: 0;
+    min-height: 473px;
+    padding: 43px 20px 20px;
+
+    .account-dh-name {
+      margin-bottom: 0;
+    }
+
+    .account-dh-login {
+      padding-bottom: 15px;
+      border-bottom: 1px solid #EEEEEE;
+      width: 100%;
+      text-align: center;
+    }
+
+    button {
+      padding: 2px 26px;
+      width: 100%;
+
+      & + button {
+        margin-top: 8px;
+      }
+    }
+  }
+
+  .account-plan {
+    text-align: center;
+    margin-top: 11px;
+    font-weight: bold;
+    font-size: 15px;
+
+    .subscription-type {
+      margin: 10px 0;
+    }
   }
 }
 </style>
