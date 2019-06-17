dcadb43f02a4709b7a3eb1c974af0aed9e90bb2a
Andrey Matukhnov <dronic.x@gmail.com>
2019-06-17 10:10:28 +0200

    Init PouchDB

--- assets/js/src/routes/accountDashboard.vue
+++ assets/js/src/routes/accountDashboard.vue
@@ -9,7 +9,7 @@
           <div>{{ account.followerCount || 0 }}<br/>followers </div>
         </div>
       </div>
-      <div class="account-info"> 
+      <div class="account-info">
         <div class="account-name">{{ account.fullName }}<div class="account-post-count"><strong>{{ account.postCount }}</strong> posts</div> </div>
         <div class="account-bio">{{ account.bio }}</div>
         <a class="account-website" :href="account.website" target="_blank">{{ account.website }}</a>
@@ -19,11 +19,25 @@
 </template>
 
 <script>
+import PouchDB from 'pouchdb';
+
 export default {
   computed: {
     account() {
       return this.$store.state.currentAccount
     }
+  },
+
+  created() {
+    const { dhAccount } = this;
+
+    //const db = new PouchDB(`http://couchdb.app02.beta.directheroes.com:5984/dronic`)
+    const db = new PouchDB(`dronic`)
+
+    db.info().then(info => {
+      console.log(info);
+
+    })
   }
 }
 </script>
@@ -51,7 +65,7 @@ export default {
 
     .account-name {
       font-size: 18px;
-      line-height: 22px; 
+      line-height: 22px;
       color: #3C3C3C;
       display: flex;
       align-items: center;
