387f5cd5efa9a308331fba15f25d51bdc0a21d14
Andrey Matukhnov <dronic.x@gmail.com>
2019-07-08 09:12:30 +0200

    Encore update

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 var Encore = require('@symfony/webpack-encore');
 const webpack = require('webpack');
 var dotenv = require('dotenv');
-dotenv.config();
+
+console.log(dotenv.config());
 
 Encore
     // directory where compiled assets will be stored
@@ -11,37 +12,14 @@ Encore
     // only needed for CDN's or sub-directory deploy
     .setManifestKeyPrefix('build/')
 
-    /*
-     * ENTRY CONFIG
-     *
-     * Add 1 entry for each "page" of your app
-     * (including one that's included on every page - e.g. "app")
-     *
-     * Each entry will result in one JavaScript file (e.g. app.js)
-     * and one CSS file (e.g. app.css) if you JavaScript imports CSS.
-     */
     .addEntry('app', './assets/js/src/main.js')
     .addEntry('register', './assets/js/src/registration/registration.js')
-
-
-    /*
-     * FEATURE CONFIG
-     *
-     * Enable & configure other features below. For a full
-     * list of features, see:
-     * https://symfony.com/doc/current/frontend.html#adding-more-features
-     */
     .enableVueLoader()
     .addPlugin(new webpack.NormalModuleReplacementPlugin(/element-ui[\/\\]lib[\/\\]locale[\/\\]lang[\/\\]zh-CN/, 'element-ui/lib/locale/lang/en'))
     .cleanupOutputBeforeBuild()
     .enableBuildNotifications()
     .enableSourceMaps(!Encore.isProduction())
-    // enables hashed filenames (e.g. app.abc123.css)
     .enableVersioning(Encore.isProduction())
-    // always enable versioning
-    // .enableVersioning(true)
-
-    // enables Sass/SCSS support
     .enableSassLoader()
     .disableCssExtraction()
     .configureBabel(() => {}, {
@@ -49,12 +27,6 @@ Encore
         corejs: 2
     })
     .disableSingleRuntimeChunk()
-
-    // uncomment if you use TypeScript
-    //.enableTypeScriptLoader()
-
-    // uncomment if you're having problems with a jQuery plugin
-    //.autoProvidejQuery()
 ;
 
 module.exports = Encore.getWebpackConfig();
