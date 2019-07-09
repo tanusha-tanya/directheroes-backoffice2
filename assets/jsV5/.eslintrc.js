991b1e07705bf69c7fab5dd38df139f610e4c7bc
Andrey Matukhnov <dronic.x@gmail.com>
2019-07-09 18:22:48 +0200

    Remove readme

--- assets/jsV5/.eslintrc.js
+++ assets/jsV5/.eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
+  extends: [
     'plugin:vue/essential',
     'eslint:recommended'
   ],
@@ -12,6 +12,8 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   parserOptions: {
-    parser: 'babel-eslint'
-  }
+    parser: 'babel-eslint',
+    sourceType: "module"
+  },
+  plugins: ['vue'],
 }
