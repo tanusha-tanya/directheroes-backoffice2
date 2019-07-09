5fedddc67f08475358eb3268794c585c5ab7b444
Andrey Matukhnov <dronic.x@gmail.com>
2019-07-09 17:36:49 +0200

    Add V5 template toencore

--- webpack.config.js
+++ webpack.config.js
@@ -8,98 +8,72 @@ let config = null;
 dotenv.config();
 
 Encore
-    .addEntry('app', './assets/js/src/main.js')
-    .addEntry('register', './assets/js/src/registration/registration.js')
-    .enableVueLoader()
-    .enableSassLoader()
-    .enablePostCssLoader(options => {
-        options.config = {
-            path: './assets/jsV5'
-        }
-    })
-    .disableCssExtraction()
-    .configureBabel(() => {}, {
-        useBuiltIns: 'usage', // or try "usage"
-        corejs: 2
-    })
-    .disableSingleRuntimeChunk()
-    .addPlugin(new webpack.NormalModuleReplacementPlugin(/element-ui[\/\\]lib[\/\\]locale[\/\\]lang[\/\\]zh-CN/, 'element-ui/lib/locale/lang/en'))
+  .addEntry('app', './assets/jsV5/src/main.js')
+  .addEntry('register', './assets/jsV5/src/registration/registration.js')
+  .enableVueLoader()
+  .enableSassLoader()
+  .enablePostCssLoader(options => {
+    options.config = {
+      path: './assets/jsV5'
+    }
+  })
+  .disableCssExtraction()
+  .configureBabel(() => { }, {
+    useBuiltIns: 'usage', // or try "usage"
+    corejs: 2
+  })
+  .disableSingleRuntimeChunk()
+  .addPlugin(new webpack.NormalModuleReplacementPlugin(/element-ui[\/\\]lib[\/\\]locale[\/\\]lang[\/\\]zh-CN/, 'element-ui/lib/locale/lang/en'))
 
 if (Encore.isProduction()) {
-    Encore
-        .setOutputPath('public/build.tmp/')
-        .setPublicPath(process.env.PUBLIC_SCHEME + '://' + process.env.VIRTUAL_HOST + '/build/')
-        .setManifestKeyPrefix('build/')
-        .cleanupOutputBeforeBuild()
-        .enableSourceMaps()
-        .enableVersioning()
+  Encore
+    .setOutputPath('public/build.tmp/')
+    .setPublicPath(process.env.PUBLIC_SCHEME + '://' + process.env.VIRTUAL_HOST + '/build/')
+    .setManifestKeyPrefix('build/')
+    .cleanupOutputBeforeBuild()
+    .enableSourceMaps()
+    .enableVersioning()
 
-    config = Encore.getWebpackConfig();
+  config = Encore.getWebpackConfig();
 } else {
-    Encore
-        .setOutputPath('./assets/js/dist')
-        .setPublicPath('/dist/')
-        // .enableEslintLoader(config => {
-        //     config.configFile = './assets/jsV5/.eslintrc.js';
-        // });
+  Encore
+    .setOutputPath('./assets/jsV5/dist')
+    .setPublicPath('/dist/')
+  // .enableEslintLoader(config => {
+  //     config.configFile = './assets/jsV5/.eslintrc.js';
+  // });
 
-    config = Encore.getWebpackConfig()
+  config = Encore.getWebpackConfig()
 
-    if (config.devServer) {
-        Object.assign(config.devServer, {
-            contentBase: path.resolve(__dirname, './assets/js'),
-            historyApiFallback: {
-                rewrites: [
-                    { from: /^\/$/, to: './index.html' },
-                    { from: /^\/register/, to: './register.html' },
-                ],
-            },
-            proxy: [{
-                context: ['/api/1.0.0/**', '/api/2.0.0/**', '/ig/accounts/**'],
-                // target: 'https://www.directheroes.com/app_dev.php',
-                // target: 'https://staging.directheroes.com/app_dev.php',
-                target: 'https://beta2.directheroes.com/app_dev.php',
-                // target: 'https://beta.directheroes.com/app_dev.php',
-                changeOrigin: true,
-                secure: true,
-                logLevel: 'debug',
-            }],
-            disableHostCheck: true,
-            inline: true,
-            overlay: {
-                warnings: true,
-                errors: true
-            }
-        })
-    }
+  if (config.devServer) {
+    Object.assign(config.devServer, {
+      contentBase: path.resolve(__dirname, './assets/jsV5'),
+      historyApiFallback: {
+        rewrites: [
+          { from: /^\/$/, to: './public/index.html' },
+          { from: /^\/register/, to: './public/register.html' },
+        ],
+      },
+      proxy: [{
+        context: ['/api/1.0.0/**', '/api/2.0.0/**', '/ig/accounts/**'],
+        // target: 'https://www.directheroes.com/app_dev.php',
+        // target: 'https://staging.directheroes.com/app_dev.php',
+        target: 'https://beta2.directheroes.com/app_dev.php',
+        // target: 'https://beta.directheroes.com/app_dev.php',
+        changeOrigin: true,
+        secure: true,
+        logLevel: 'debug',
+      }],
+      disableHostCheck: true,
+      inline: true,
+      overlay: {
+        warnings: true,
+        errors: true
+      }
+    })
+  }
 
-    config.plugins.push(new webpack.HotModuleReplacementPlugin())
+  config.plugins.push(new webpack.HotModuleReplacementPlugin())
 }
-
-
-    // Encore
-//     // directory where compiled assets will be stored
-//     .setOutputPath('public/build.tmp/')
-//     // public path used by the web server to access the output path
-//     .setPublicPath(process.env.PUBLIC_SCHEME + '://' + process.env.VIRTUAL_HOST + '/build/')
-//     // only needed for CDN's or sub-directory deploy
-//     .setManifestKeyPrefix('build/')
-
-//     .addEntry('app', './assets/js/src/main.js')
-//     .addEntry('register', './assets/js/src/registration/registration.js')
-//     .enableVueLoader()
-//
-//     .cleanupOutputBeforeBuild()
-//     .enableBuildNotifications()
-//     .enableSourceMaps(!Encore.isProduction())
-//     .enableVersioning(Encore.isProduction())
-//     .enableSassLoader()
-//     .disableCssExtraction()
-//     .configureBabel(() => {}, {
-//         useBuiltIns: 'usage', // or try "usage"
-//         corejs: 2
-//     })
-//     .disableSingleRuntimeChunk()
-// ;
 
 module.exports = config;
