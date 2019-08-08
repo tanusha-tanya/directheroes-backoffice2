b4c27113543f6c003beafe83cf86df0490552fe6
Kirill Kosorukov <kkosorukov.ecredit@gmail.com>
2019-08-08 22:47:04 +0300

    EasyWebinar link

---
+++ assets/js/src/routes/easyWebinar.vue
@@ -0,0 +1,14 @@
+<template></template>
+
+<script>
+  export default {
+
+    beforeRouteEnter(to, from, next) {
+      let url = `${ dh.easywebinarLink }/oneclick-registration?attendee_name=${ dh.userName }&attendee_email=${ dh.userName }`
+      window.open(url, "Webinar join", "width=800,height=700,top=200,left=200");
+    }
+
+  }
+</script>
+
+<style lang="scss"></style>
