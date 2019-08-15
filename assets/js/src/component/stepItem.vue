be7840b5c0b5d6ed6bf60684250f6987db2be92b
Andrey Matukhnov <dronic.x@gmail.com>
2019-08-15 20:39:48 +0200

    Add action linker

--- assets/js/src/component/stepItem.vue
+++ assets/js/src/component/stepItem.vue
@@ -1,12 +1,15 @@
 <template>
 <div :class="[{'step-item': true}, `step-${ stepType }-type`]" @mousedown="blockEvent">
-  <div class="step-item-header">
+  <div class="step-item-header" :ref="stepType === 'action' && linker && linker.id">
     <span>
       {{flowName || step.name || '&nbsp;'}}
     </span>
-    <div class="step-delete-button" v-if="!isEntry && !hasChilds" @click="$emit('delete-step', step)">
-      <svg viewBox="0 0 21 20" xmlns="http://www.w3.org/2000/svg"><path d="M7.35 16h2.1V8h-2.1v8zm4.2 0h2.1V8h-2.1v8zm-6.3 2h10.5V6H5.25v12zm2.1-14h6.3V2h-6.3v2zm8.4 0V0H5.25v4H0v2h3.15v14h14.7V6H21V4h-5.25z" fill="currentColor" fill-rule="evenodd"/></svg>
-    </div>
+    <span>
+      <div class="step-delete-button" v-if="!isEntry && !hasChilds" @click="$emit('delete-step', step)">
+        <svg viewBox="0 0 21 20" xmlns="http://www.w3.org/2000/svg"><path d="M7.35 16h2.1V8h-2.1v8zm4.2 0h2.1V8h-2.1v8zm-6.3 2h10.5V6H5.25v12zm2.1-14h6.3V2h-6.3v2zm8.4 0V0H5.25v4H0v2h3.15v14h14.7V6H21V4h-5.25z" fill="currentColor" fill-rule="evenodd"/></svg>
+      </div>
+      <add-step-popup :available-list="availableList" @add-step="createStep" v-if="stepType === 'action' && !linker"></add-step-popup>
+    </span>
   </div>
   <component :is="stepType" :is-entry="isEntry" :elements="step.elements" @add-step="$emit('add-step', $event)"></component>
 </div>
@@ -17,6 +20,8 @@ import action from './elements/action'
 import rule from './elements/rule'
 import message from './elements/message'
 import utils from '../utils'
+import ObjectId from '../utils/ObjectId';
+import addStepPopup from './addStepPopup';
 
 export default {
   props: ['step', 'flowName'],
@@ -24,7 +29,8 @@ export default {
   components: {
     message,
     action,
-    rule
+    rule,
+    addStepPopup
   },
 
   computed: {
@@ -40,16 +46,53 @@ export default {
 
       return elements.some(element => {
         const matchElement = utils.getOnMatchElement(element);
+        const target =  matchElement && (matchElement.target || (matchElement.onMatch && matchElement.onMatch.target))
 
-        return matchElement && matchElement.onMatch;
+        return target;
       })
+    },
+
+    availableList() {
+      const { isEntry } = this;
+      const { messageTypes } = this.dhAccount.flowBuilderSettings[isEntry ? 'growthTools': 'triggers'];
+
+      return ['sendText', 'sendMedia'].concat(messageTypes);
     },
 
     isEntry() {
       const { displaySettings } = this.step;
 
       return displaySettings && displaySettings.isEntry;
+    },
+
+    linker() {
+      const { elements } = this.step;
+
+      return elements.find(element => element.type === 'linker');
     }
+  },
+
+  methods: {
+    createStep(element) {
+      const { elements } = this.step;
+      const step = {
+        id: (new ObjectId).toString(),
+        elements: [
+          {
+            id: (new ObjectId).toString(),
+            ...element
+          }
+        ]
+      }
+
+      elements.push({
+        id: (new ObjectId).toString(),
+        type: 'linker',
+        target: step.id
+      })
+
+      this.$emit('add-step', step);
+    },
   }
 }
 </script>
@@ -74,18 +117,56 @@ export default {
       display: flex;
       justify-content: space-between;
       align-items: center;
+      position: relative;
+    }
+
+    .add-step-button {
+      position: absolute;
+      right: -14px;
+      top: calc(50% - 14px);
+
+      &:after {
+        content: '';
+        position: absolute;
+        top: calc(50% - 1px);
+        left: calc(50% - 7px);
+        height: 2px;
+        background-color: #ccc;
+        width: 14px;
+      }
+
+      &:before {
+        content: '';
+        position: absolute;
+        top: calc(50% - 7px);
+        left: calc(50% - 1px);
+        height: 14px;
+        background-color: #ccc;
+        width: 2px;
+      }
+
+      &:hover {
+        &:after, &:before {
+          background-color: #6A12CB;
+        }
+      }
     }
 
     .step-delete-button {
       color: #A9A9A9;
       width: 13px;
-    }
+
+     }
 
     &.step-action-type {
       .step-item-header {
         color: #F4B109;
         background-color: rgba(#F4B109, .25);
         border: 1px solid #F4B109;
+      }
+
+      .step-delete-button {
+        margin-right: 10px;
       }
     }
 
