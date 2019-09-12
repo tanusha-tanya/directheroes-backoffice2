ec4728c087a9d36ec8e34e955dbe17c2a8e67152
Andrey Matukhnov <dronic.x@gmail.com>
2019-09-12 10:33:32 +0200

    To last commit

---
+++ assets/jsV5/src/components/dh-confirm-dialog.vue
@@ -0,0 +1,49 @@
+<template>
+  <el-dialog
+    :visible.sync="isShow"
+    :title="title"
+    width="554px"
+    append-to-body
+    class="dh-confirm-dialog"
+  >
+    <div class="dialog-text">{{ message }}</div>
+    <template slot="footer">
+      <button class="dh-button" @click="$emit('success')">Delete</button>
+      <button class="dh-button dh-reset-button" @click="cancelHandler">Cancel</button>
+    </template>
+  </el-dialog>
+</template>
+
+<script>
+export default {
+  props: ['value', 'message', 'title'],
+
+  computed: {
+    isShow: {
+      get() {
+        return this.value
+      },
+
+      set(value) {
+        this.$emit('input', value)
+      }
+    }
+  },
+
+  methods: {
+    cancelHandler() {
+      this.$emit('cancel'),
+      this.isShow = false
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .dh-confirm-dialog {
+    .el-dialog__footer {
+      display: flex;
+      justify-content: space-between;
+    }
+  }
+</style>
