e1bdb74f849cd24ac8aae2d64211cb96f7ec17cb
Andrey Matukhnov <dronic.x@gmail.com>
2019-09-24 00:58:05 +0200

    Fix cookie

--- assets/js/src/component/stripePayment.vue
+++ assets/js/src/component/stripePayment.vue
@@ -238,8 +238,7 @@ export default {
     const referrer = /ref=(\w*)/.exec(location.search);
 
     if (referrer && referrer[1]) {
-      document.cookie = document.cookie + (document.cookie ? '; ' : '') + ('referrer='+referrer[1]+';')
-      console.log(document.cookie);
+      document.cookie = 'referrer='+referrer[1]
     }
 
     if (stripePk) {
