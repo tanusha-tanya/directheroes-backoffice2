c78f66a972c41947e23dc2a53b9c666aacfd02cd
Andrey Matukhnov <dronic.x@gmail.com>
2019-10-27 15:26:15 +0200

    Account changes

---
+++ assets/jsV5/src/components/dh-connection-wizzard.vue
@@ -0,0 +1,41 @@
+<template>
+   <el-dialog
+    :visible.sync="isShow"
+    width="554px"
+    append-to-body
+    class="dh-wizzard-dialog"
+  >
+    <div class="dh-wizzard-steps">
+      <component :is="wizzardState"></component>
+    </div>
+    <template slot="footer">
+      <button class="dh-button dh-danger-button" @click="$emit('success')">Deactivate campaign</button>
+    </template>
+  </el-dialog>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+
+  props: ['authAccount'],
+
+  computed: {
+    wizzardState() {
+      const { authAccount } = this;
+
+      if (!authAccount) {
+        return 'selectAccount'
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ assets/jsV5/src/components/dh-connection-wizzard/select-account.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="dh-select-account">
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+.dh-select-account {
+
+}
+</style>
