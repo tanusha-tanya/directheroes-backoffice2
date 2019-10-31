4b4fa12ff36905dcd6799a59ad8f18cd735cf9f2
Andrey Matukhnov <dronic.x@gmail.com>
2019-10-31 16:38:04 +0200

    Add zapier after tag

--- assets/js/src/elements/permissions.js
+++ assets/js/src/elements/permissions.js
@@ -3,7 +3,7 @@ export default {
   fromTrigger: ['sendMedia', 'sendText', 'delay', 'delayTill', 'addCategory', 'removeCategory', 'followers', 'zapier', 'verified', 'topCategory', 'scarcity'],
   fromTriggerStep: [],
   fromMessageStep: ['sendText', 'sendMedia', 'delay', 'delayTill', 'addCategory', 'timeout', 'user-input', 'followers', 'zapier', 'verified', 'topCategory', 'scarcity'],
-  fromActionStep: ['sendText', 'sendMedia', 'delay', 'delayTill', 'followers', 'timeout', 'addCategory', 'existingStep', 'verified', 'topCategory', 'scarcity'],
+  fromActionStep: ['sendText', 'sendMedia', 'delay', 'delayTill', 'followers', 'timeout', 'addCategory', 'existingStep', 'verified', 'topCategory', 'scarcity', 'zapier'],
   fromCondition: ['sendText', 'sendMedia', 'delay', 'delayTill', 'followers', 'addCategory', 'zapier', 'verified', 'topCategory', 'scarcity'],
   fromUserInputFails: ['sendText', 'sendMedia', 'delay', 'delayTill', 'timeout', 'followers', 'addCategory', 'verified', 'topCategory', 'scarcity'],
   fromUserInput: ['sendText', 'sendMedia', 'delay', 'delayTill', 'followers', 'zapier', 'addCategory', 'removeCategory', 'verified',  'topCategory', 'scarcity'],
