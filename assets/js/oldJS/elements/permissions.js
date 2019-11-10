1ddc1f0a198fe5ea0892237aebc225a50ca8117a
Andrey Matukhnov <dronic.x@gmail.com>
2019-11-10 22:03:37 +0200

    Add timeout for condition

--- assets/js/oldJS/elements/permissions.js
+++ assets/js/oldJS/elements/permissions.js
@@ -4,7 +4,7 @@ export default {
   fromTriggerStep: [],
   fromMessageStep: ['sendText', 'sendMedia', 'delay', 'delayTill', 'addCategory', 'timeout', 'user-input', 'followers', 'zapier', 'verified', 'topCategory', 'scarcity', 'hasTag', 'waitTillCondition'],
   fromActionStep: ['sendText', 'sendMedia', 'delay', 'delayTill', 'followers', 'timeout', 'addCategory', 'existingStep', 'verified', 'topCategory', 'scarcity', 'zapier', 'hasTag', 'waitTillCondition'],
-  fromCondition: ['sendText', 'sendMedia', 'delay', 'delayTill', 'followers', 'addCategory', 'zapier', 'verified', 'topCategory', 'scarcity', 'hasTag', 'waitTillCondition'],
+  fromCondition: ['sendText', 'sendMedia', 'delay', 'delayTill', 'followers', 'addCategory', 'zapier', 'verified', 'topCategory', 'scarcity', 'hasTag', 'waitTillCondition', 'timeout'],
   fromUserInputFails: ['sendText', 'sendMedia', 'delay', 'delayTill', 'timeout', 'followers', 'addCategory', 'verified', 'topCategory', 'scarcity', 'hasTag', 'waitTillCondition'],
   fromUserInput: ['sendText', 'sendMedia', 'delay', 'delayTill', 'followers', 'zapier', 'addCategory', 'removeCategory', 'verified',  'topCategory', 'scarcity', 'hasTag', 'waitTillCondition'],
   fromBroadcastFlow: ['sendText', 'sendMedia', 'delayTill', 'followers', 'verified', 'topCategory', 'hasTag', 'waitTillCondition'],
