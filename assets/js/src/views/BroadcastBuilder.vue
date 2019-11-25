42b344a14ced5705c4023abdb526d1749e1e4c5d
Andrey Matukhnov <dronic.x@gmail.com>
2019-11-26 01:00:00 +0200

    Broadcast send test message

--- assets/js/src/views/BroadcastBuilder.vue
+++ assets/js/src/views/BroadcastBuilder.vue
@@ -24,59 +24,24 @@
             </div>
           </div>
         </div>
-        <div class="dh-campaign-test" v-if="false">
+        <div class="dh-campaign-test">
           <button class="dh-button dh-small" :disabled="hasWarning" @click="isTestCampaign = true">Test broadcast</button>
         </div>
-        <span></span>
       </div>
     </dh-header>
     <div class="dh-view-content">
       <old-broadcast-builder ref="oldBuilder" :has-warning="hasWarning" :current-broadcast="currentCampaign"></old-broadcast-builder>
     </div>
     <dh-footer></dh-footer>
-    <el-dialog
-      :visible.sync="isTestCampaign"
-      title="Test Broadcast"
-      width="554px"
-      append-to-body
-      class="dh-campaign-test-dialog"
-    >
-      <div class="dh-campaign-test-body">
-        <el-select
-          v-model="selectedSubscriber"
-          remote
-          filterable
-          placeholder="Please enter a subscriber name"
-          :remote-method="getSubscribers"
-          :loading="searching">
-          <el-option
-            v-for="subscriber in subscribers"
-            :key="subscriber.id"
-            :label="subscriber.username"
-            :value="subscriber.id">
-            <div class="dh-subscriber-item">
-              <div class="dh-subscriber-userpic" :style="{'background-image': `url(${ subscriber.contactProfile.profilePicUrl })`}"></div>
-              <div class="dh-subscriber-info">
-                <div class="dh-subscriber-name">{{subscriber.contactProfile.fullName}}</div>
-                <div class="dh-subscriber-login">@{{subscriber.contactProfile.username}}</div>
-              </div>
-            </div>
-          </el-option>
-        </el-select>
-      </div>
-      <template slot="footer">
-        <span></span>
-        <button class="dh-button dh-reset-button" :disabled="sending" @click="isTestCampaign = false">Cancel</button>
-      </template>
-  </el-dialog>
+    <dh-campaign-test-dialog v-model="isTestCampaign" v-if="isTestCampaign" :campaign-id="currentCampaign.id"></dh-campaign-test-dialog>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
 import moment from 'moment'
 import dhHeader from '../components/dh-header'
 import dhFooter from '../components/dh-footer'
+import dhCampaignTestDialog from '../components/dh-campaign-test-dialog'
 import gear from '../assets/gear.svg'
 
 import OldBroadcastBuilder from '../../oldJS/routes/broadcastBuilder'
@@ -101,13 +66,8 @@ export default {
   data() {
     return {
       isTestCampaign: false,
-      sending: false,
-      subscribers: [],
-      searching: false,
       currentCampaign: null,
       builder: null,
-      selectedSubscriber: null,
-      source: null,
     }
   },
 
@@ -115,15 +75,12 @@ export default {
     dhHeader,
     dhFooter,
     OldBroadcastBuilder,
+    dhCampaignTestDialog,
     gear,
-    triangle
+    triangle,
   },
 
   computed: {
-    accountId() {
-      return this.$route.params.accountId
-    },
-
     hasWarning() {
       const { currentCampaign } = this;
 
@@ -181,38 +138,6 @@ export default {
     timeToStart(startAt) {
       return `${moment().from(new Date(startAt), true)} to start`
     },
-
-    getSubscribers(keyword) {
-      const { source, accountId } = this;
-      const CancelToken = axios.CancelToken;
-
-      this.searching = true;
-
-      if (source) {
-        source.cancel('Lalala')
-      }
-
-      this.source = CancelToken.source();
-
-      axios({
-        url: `${ dh.apiUrl }/api/1.0.0/${ dh.userName }/thread/list/ig_account/${ accountId }/audience`,
-        method: 'post',
-        data: {
-          usernameQuery: keyword,
-          subscribed: 'all',
-          paging: {
-            page: 1,
-            totalPageCount: 1
-          }
-        },
-        cancelToken: this.source.token
-      })
-      .then(({ data }) => {
-        this.searching = false;
-
-        this.subscribers = data.response.body.threadList;
-      }).catch(()=> {});
-    }
   },
 
   watch:{
@@ -296,57 +221,6 @@ export default {
 .dh-campaign-settings {
   .el-switch{
     margin-right: 10px;
-  }
-}
-
-.dh-subscriber-item {
-  display: flex;
-  align-items: center;
-
-  .dh-subscriber-userpic {
-    width: 30px;
-    height: 30px;
-    background-color: rgba($borderColor, .5);
-    border-radius: 50%;
-    background-position: center;
-    background-size: cover;
-    flex-shrink: 0;
-    margin-right: 16px;
-  }
-
-  .dh-subscriber-info {
-    line-height: normal;
-    color: #252631;
-    margin-top: 2px;
-  }
-
-  .dh-subscriber-login {
-    color: #98A9BC;
-    font-size: 12px;
-  }
-}
-
-.dh-campaign-test-dialog {
-  .el-select {
-    width: 100%;
-    .el-input {
-      .el-input__inner {
-        background-color: $sectionBG;
-        border: 1px solid $borderColor;
-        border-radius: 4px;
-        padding: 14px 19px 16px 17px;
-        line-height: 17px;
-        font-weight: 500;
-        color: $inputTextColor;
-        outline: none;
-      }
-
-      &.is-focus {
-        .el-input__inner {
-          border-color: #9E4CF9;
-        }
-      }
-    }
   }
 }
 </style>
