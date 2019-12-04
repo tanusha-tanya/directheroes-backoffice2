cc435c3ddda6ffe68d6edcbd96d370266144c26b
Andrey Matukhnov <dronic.x@gmail.com>
2019-12-04 18:39:20 +0200

    Change affiliate page

--- assets/js/src/views/Affiliate.vue
+++ assets/js/src/views/Affiliate.vue
@@ -1,63 +1,73 @@
 <template>
   <div class="dh-view dh-affiliate-view">
-    <dh-header title="Affiliate"></dh-header>
+    <dh-header title="Partner Program"></dh-header>
     <div class="dh-view-content">
-      <div class="dh-affiliate" v-if="affiliateInfo">
-        <div class="dh-affiliate-row dh-affiliate-info">
-          <div class="dh-affiliate-column">
-            <div>We will send affiliate payouts via Paypal to:</div>
-            <div>
-              <input type="text" class="dh-input" placeholder="Enter Paypal Email" v-model="affiliateInfo.settings.payoutEmail" @input="payoutEmailChanged = true"/>
-              <button v-if="payoutEmailChanged" :class="{'dh-button':true, 'dh-small':true, 'dh-loading':saving}" :disabled="!affiliateInfo.settings.payoutEmail" @click="saveAffiliateInfo">Save</button>
-            </div>
-            <div>Payouts will be made on the first of each month.</div>
-          </div>
-          <div class="dh-affiliate-column">
-            <div>Your referral link:</div>
-            <div class="dh-affiliate-link">
-              {{dh.apiUrl}}/check-out/?ref=
-              <input type="text" class="dh-input" @input="referralClean" v-model="affiliateInfo.settings.referrerCode"/>
-              <button v-if="referrerCodeChanged" :class="{'dh-button':true, 'dh-small':true, 'dh-loading':saving}" :disabled="!affiliateInfo.settings.referrerCode" @click="saveAffiliateInfo">Save</button>
-              <button v-else class="dh-button dh-small dh-link-button" @click="copyRefLink">
-                <dh-link/>
-              </button>
-            </div>
-            <div>Please use this for all referrals.</div>
-          </div>
-        </div>
-        <div class="dh-affiliate-row">
-          <div class="dh-affiliate-column">
-            <div class="dh-affiliate-column-title">Affiliate Statistics:</div>
-            <div>Total referrals: {{ affiliateInfo.report.length }}</div>
-            <div>Active referrals: {{ affiliateInfo.report.filter(reportItem => reportItem.referredUserIsSubscribed).length }}</div>
-          </div>
-          <div class="dh-affiliate-column">
-            <div class="dh-affiliate-column-title">Payout Statistics:</div>
-            <div>Total Earnings: 0</div>
-            <div>Total Unpaid: 0</div>
-          </div>
-        </div>
-        <div class="dh-affiliate-row">
-          <div class="dh-affiliate-column" >
-            <div class="dh-affiliate-column-title">Referral users</div>
-            <div class="dh-referral-user">
-              <div class="dh-referral-user-item dh-referral-user-header">
-                <div class="dh-referral-user-email">User</div>
-                <div class="dh-referral-user-bounty">Total Earned</div>
-                <div class="dh-referral-user-joined-at">Start Date</div>
-                <div class="dh-referral-user-subscribe">Status</div>
+      <template v-if="dhAccount.features.affilate">
+        <div class="dh-affiliate" v-if="affiliateInfo">
+          <div class="dh-affiliate-row dh-affiliate-info">
+            <div class="dh-affiliate-column">
+              <div>We will send affiliate payouts via Paypal to:</div>
+              <div>
+                <input type="text" class="dh-input" placeholder="Enter Paypal Email" v-model="affiliateInfo.settings.payoutEmail" @input="payoutEmailChanged = true"/>
+                <button v-if="payoutEmailChanged" :class="{'dh-button':true, 'dh-small':true, 'dh-loading':saving}" :disabled="!affiliateInfo.settings.payoutEmail" @click="saveAffiliateInfo">Save</button>
               </div>
-              <div class="dh-referral-user-item" v-for="(reportItem, index) in affiliateInfo.report" :key="reportItem.referredUserJoinedAt + index">
-                <div class="dh-referral-user-email">{{reportItem.referredUserEmailMasked}}</div>
-                <div class="dh-referral-user-bounty">{{reportItem.allTimeAffiliateBounty}}</div>
-                <div class="dh-referral-user-joined-at">{{dateFormated(reportItem.referredUserJoinedAt)}}</div>
-                <div class="dh-referral-user-subscribe">{{reportItem.referredUserIsSubscribed ? 'Active' : 'Inactive'}}</div>
+              <div>Payouts will be made on the first of each month.</div>
+            </div>
+            <div class="dh-affiliate-column">
+              <div>Your referral link:</div>
+              <div class="dh-affiliate-link">
+                {{dh.apiUrl}}/check-out/?ref=
+                <input type="text" class="dh-input" @input="referralClean" v-model="affiliateInfo.settings.referrerCode"/>
+                <button v-if="referrerCodeChanged" :class="{'dh-button':true, 'dh-small':true, 'dh-loading':saving}" :disabled="!affiliateInfo.settings.referrerCode" @click="saveAffiliateInfo">Save</button>
+                <button v-else class="dh-button dh-small dh-link-button" @click="copyRefLink">
+                  <dh-link/>
+                </button>
+              </div>
+              <div>Please use this for all referrals.</div>
+            </div>
+          </div>
+          <div class="dh-affiliate-row">
+            <div class="dh-affiliate-column">
+              <div class="dh-affiliate-column-title">Referral Statistics:</div>
+              <div>Total referrals: {{ affiliateInfo.report.length }}</div>
+              <div>Active referrals: {{ affiliateInfo.report.filter(reportItem => reportItem.referredUserIsSubscribed).length }}</div>
+            </div>
+            <div class="dh-affiliate-column">
+              <div class="dh-affiliate-column-title">Payout Statistics:</div>
+              <div>Total Earnings: 0</div>
+              <div>Total Unpaid: 0</div>
+            </div>
+          </div>
+          <div class="dh-affiliate-row">
+            <div class="dh-affiliate-column" >
+              <div class="dh-affiliate-column-title">Referral users</div>
+              <div class="dh-referral-user">
+                <div class="dh-referral-user-item dh-referral-user-header">
+                  <div class="dh-referral-user-email">User</div>
+                  <div class="dh-referral-user-bounty">Total Earned</div>
+                  <div class="dh-referral-user-joined-at">Start Date</div>
+                  <div class="dh-referral-user-subscribe">Status</div>
+                </div>
+                <div class="dh-referral-user-item" v-for="(reportItem, index) in affiliateInfo.report" :key="reportItem.referredUserJoinedAt + index">
+                  <div class="dh-referral-user-email">{{reportItem.referredUserEmailMasked}}</div>
+                  <div class="dh-referral-user-bounty">{{reportItem.allTimeAffiliateBounty}}</div>
+                  <div class="dh-referral-user-joined-at">{{dateFormated(reportItem.referredUserJoinedAt)}}</div>
+                  <div class="dh-referral-user-subscribe">{{reportItem.referredUserIsSubscribed ? 'Active' : 'Inactive'}}</div>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-      <loader v-else/>
+        <loader v-else/>
+      </template>
+      <template v-else>
+        <div class="dh-affiliate-template">
+          <div class="dh-info" >To join our partner program please send your Partners Request below</div>
+          <a class="dh-button" href="mailto:hello@directheroes.com" target="_blank">
+            Apply now
+          </a>
+        </div>
+      </template>
     </div>
     <dh-footer></dh-footer>
   </div>
@@ -235,6 +245,35 @@ export default {
 
   .dh-link-button {
     padding: 5px 7px !important;
+  }
+
+  .dh-affiliate-template {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    flex-grow: 1;
+  }
+
+  a.dh-button {
+    color: $sectionBG;
+    background-color: $elementActiveColor;
+    height: 44px;
+    font-weight: bold;
+    min-width: 206px;
+    padding: 15px 15px 14px;
+    text-transform: uppercase;
+    border-radius: 4px;
+    outline: none;
+    border: 1px solid $elementActiveColor;
+    text-decoration: none;
+    text-align: center;
+    margin-top: 20px;
+
+
+    &:hover {
+      background-color: #9248e5;
+    }
   }
 }
 </style>
