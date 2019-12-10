8d845d7c0cc72a527a8de7ecc13a8490608ac2d1
Andrey Matukhnov <dronic.x@gmail.com>
2019-12-10 23:04:06 +0200

    Fix followers count

--- assets/js/src/views/AccountDashboard.vue
+++ assets/js/src/views/AccountDashboard.vue
@@ -25,7 +25,7 @@
         <div class="dh-dashboard-analytics-item" v-if="analyticInfo.followerCount">
           <div class="dh-analytics-item-info">
             <div :class="{'dh-analytics-item-value': true,'dh-analytics-success': followerCountProgress > 0 }">
-              {{deltaFollowerCount.toLocaleString()}}
+              {{Math.floor(deltaFollowerCount)}}
               <svg width="8" height="12" viewBox="0 0 8 12" fill="none" xmlns="http://www.w3.org/2000/svg" v-if="followerCountProgress">
                 <path d="M4 0.0625L4.375 0.40625L7.375 3.40625L6.625 4.125L4.5 1.96875V12H3.5V1.96875L1.375 4.125L0.625 3.40625L3.625 0.40625L4 0.0625Z" fill="currentColor"/>
               </svg>
