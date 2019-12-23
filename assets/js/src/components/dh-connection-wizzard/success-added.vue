9c01e12fe2029822a27b5e8bfdb55d090c481df2
Andrey Matukhnov <dronic.x@gmail.com>
2019-12-23 15:40:04 +0200

    Fix account connection wizard success connection text

--- assets/js/src/components/dh-connection-wizzard/success-added.vue
+++ assets/js/src/components/dh-connection-wizzard/success-added.vue
@@ -2,7 +2,7 @@
   <div class="dh-wizzard-step dh-success-added">
     <div class="dh-wizzard-step-body">
       <div class="dh-wizzard-success" >
-        Account successfuly logged
+        Instagram account connected successfully
       </div>
     </div>
     <div class="el-dialog__footer">
