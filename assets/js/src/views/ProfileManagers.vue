70f65a21fc95c4e8820266f6df652e6d64666683
Andrey Matukhnov <dronic.x@gmail.com>
2019-12-27 17:25:35 +0200

    Add check accounts

--- assets/js/src/views/ProfileManagers.vue
+++ assets/js/src/views/ProfileManagers.vue
@@ -11,22 +11,24 @@
     <loader v-if="loading"/>
     <div class="dh-list" v-else-if="managers && managers.length">
       <div class="dh-list-item" v-for="manager in managers" :key="manager.id">
-        {{manager.username}}
+        <div class="dh-manager-item">
+          <div class="dh-manager-name">
+            <email />
+            {{manager.username}}
+          </div>
+          <button class="dh-button dh-small" @click="managerToManage = manager">
+            Manage
+          </button>
+          <div class="dh-manager-delete-button" @click="managerToDelete = manager">
+            <trash />
+          </div>
+        </div>
       </div>
     </div>
     <div class="dh-info" v-else>
       <span>
-        <strong>No managers found?</strong>
-        Click "Add manager" button to add one
+        Click <strong>"Add manager"</strong> button to add one
       </span>
-      <!-- <div class="dh-settings-form">
-        <div class="dh-settings-form-row" v-for="(manager, key) in managers" :key=key>
-          <div class="dh-label">
-            <span>Email</span>
-            {{ manager.username }}
-          </div>
-        </div>
-      </div> -->
     </div>
     <el-dialog
       :visible.sync="isAddManager"
@@ -43,8 +45,34 @@
         </div>
       </div>
       <template slot="footer">
-        <button :class="{'dh-button': true }" :disabled="!newManagerEmail" @click="addManager">Add</button>
+        <button :class="{'dh-button': true, 'dh-loading': isManagerProcess }" :disabled="!newManagerEmail" @click="addManager">Add</button>
         <button class="dh-button dh-reset-button" @click="isAddManager = false">Close</button>
+      </template>
+    </el-dialog>
+    <el-dialog
+      :visible.sync="isManagePopup"
+      v-if="isManagePopup"
+      title="Instagram accounts manager"
+      custom-class="dh-manager-accounts-dialog"
+      append-to-body
+      width="554px">
+      <div :class="{'dh-select-account-founded': true, 'dh-select-account-selected': isAccountChecked(managerToManage, account)}" v-for="account in igAccounts" @click="toggleManagerToAccount(managerToManage, account)">
+        <div class="dh-select-account-userpic" :style="{ 'background-image': `url(${ account.profilePicUrl })` }">
+        </div>
+        <div class="dh-select-account-info">
+          <div class="dh-select-account-names">
+            <strong>{{account.fullName}}</strong> @{{account.login}}
+          </div>
+          <div class="dh-select-account-follows">
+            <strong>{{account.followerCount}}</strong> followers <strong>{{account.followingCount}}</strong> following
+          </div>
+          <div class="dh-select-account-text">
+            Click here, to attach/deatach account to manager.
+          </div>
+        </div>
+      </div>
+      <template slot="footer">
+        <button class="dh-button" @click="isManagePopup = false">Ok</button>
       </template>
     </el-dialog>
     <confirm-dialog
@@ -60,6 +88,8 @@
 import axios from 'axios';
 import loader from '../components/dh-loader'
 import plus from '../assets/plus.svg'
+import trash from '../assets/trash.svg'
+import email from '../assets/email.svg'
 import confirmDialog from '../components/dh-confirm-dialog'
 
 export default {
@@ -69,7 +99,8 @@ export default {
       error: null,
       managerToDelete: null,
       isAddManager: false,
-      isAddProcess: false,
+      isManagerProcess: false,
+      managerToManage: null,
       managers: null,
       loading: true,
     }
@@ -78,7 +109,9 @@ export default {
   components: {
     loader,
     confirmDialog,
-    plus
+    plus,
+    trash,
+    email
   },
 
   computed: {
@@ -92,15 +125,33 @@ export default {
       set() {
         this.managerToDelete = null;
       }
-    }
+    },
+
+    isManagePopup: {
+      get() {
+        const { managerToManage } = this;
+
+        return Boolean(managerToManage)
+      },
+
+      set() {
+        this.managerToManage = null;
+      }
+    },
+
+    igAccounts() {
+      const { accounts } = this.$store.state;
+
+      return accounts
+    },
   },
 
   methods: {
     addManager() {
-      const { newManagerEmail } = this;
+      const { newManagerEmail, managers } = this;
 
       this.error = '';
-      this.isAddProcess = true;
+      this.isManagerProcess = true;
 
       axios({
         url: `${dh.apiUrl}/api/1.0.0/${dh.userName}/dh-account/child-user`,
@@ -111,8 +162,8 @@ export default {
       }).then(({ data }) => {
         managers.push(data.response.body.dhAccount)
 
-        this.isAddProcess = false;
         this.isAddManager = false;
+        this.isManagerProcess = false;
       }).catch((error) => {
         if (error.response && error.response.data.request) {
           this.error = error.response.data.request.statusMessage
@@ -120,24 +171,51 @@ export default {
           this.error = 'Something goes wrong. Please try again later or contact with our support.'
         }
 
-        this.isAddProcess = false;
+        this.isManagerProcess = false;
       })
     },
 
     deleteManager() {
-      this.deleting = true
-      axios.delete(`${dh.apiUrl}/api/1.0.0/${dh.userName}/dh-account/child-user/${this.managerToDelete.id}`
+      const { managers, managerToDelete } = this;
+
+      axios.delete(`${dh.apiUrl}/api/1.0.0/${dh.userName}/dh-account/child-user/${managerToDelete.id}`
       ).then(() => {
-        this.getManagers()
-        this.deleting = false
+        managers.splice(managers.indexOf(managerToDelete), 1)
+
+        this.managerToDelete = null
       }).catch(()=>{
-        this.deleting = false
+        this.managerToDelete = null
       })
-      this.cancelDelete()
     },
-    cancelDelete() {
-      this.managerToDelete = {}
-      this.deleting = false
+
+    isAccountChecked(manager, account) {
+      const { igAccounts } = manager;
+
+      return igAccounts.find(igAccount => igAccount.id === account.id)
+    },
+
+    toggleManagerToAccount(manager, account) {
+      const { isAccountChecked, } = this;
+      const isChecked = isAccountChecked(manager, account);
+
+      if (isChecked) {
+        const igAccount = manager.igAccounts.find(igAccount => igAccount.id === account.id);
+
+        manager.igAccounts.splice(manager.igAccounts.indexOf(igAccount), 1)
+      } else {
+        manager.igAccounts.push(account);
+      }
+
+      axios({
+        url: `${dh.apiUrl}/api/1.0.0/${dh.userName}/ig_account/${account.id}`,
+        method: 'post',
+        data: {
+          secondaryOwners: {
+            action: isChecked ? "remove" : "add",
+            list: [manager.id]
+          }
+        }
+      })
     }
   },
 
@@ -145,7 +223,7 @@ export default {
     axios({
       url: `${dh.apiUrl}/api/1.0.0/${dh.userName}/dh-account/child-user`
     }).then(({data}) => {
-      this.managers = data.response.body.list;
+      this.managers = data.response.body.list || [];
       this.loading = false;
     })
   },
@@ -165,6 +243,38 @@ export default {
     display: flex;
     justify-content: flex-end;
   }
+
+  .dh-list-item {
+    cursor: default;
+  }
+
+  .dh-manager-item {
+    display: flex;
+    align-items: center;
+    width: 100%;
+
+    .dh-manager-name {
+      flex-grow: 1;
+      display: flex;
+      align-items: center;
+
+      svg {
+        width: 20px;
+        color: #98A9BC;
+        margin-right: 15px;
+      }
+    }
+
+    .dh-button {
+      margin-right: 25px;
+      cursor: pointer;
+    }
+
+    .dh-manager-delete-button {
+      color: #98A9BC;
+      cursor: pointer;
+    }
+  }
 }
 
 .dh-manager-add-dialog {
@@ -174,6 +284,40 @@ export default {
 
   .el-dialog__body {
     min-height: 200px;
+  }
+}
+
+.dh-manager-accounts-dialog {
+  .el-dialog__body {
+    max-height: 400px;
+    overflow: auto;
+  }
+
+  .dh-select-account-founded {
+    height: 60px;
+    margin-top: 0;
+    margin-bottom: 5px;
+
+    &.dh-select-account-selected {
+      position: relative;
+
+      &:before {
+        position: absolute;
+        content: '';
+        left: 7px;
+        top: calc(50% - 5px);
+        width: 10px;
+        height: 5px;
+        border-style: solid;
+        border-color: #6DD230;
+        border-width: 0 0 2px 2px;
+        transform: rotate(-45deg)
+      }
+    }
+  }
+
+  .el-dialog__footer {
+    justify-content: flex-end !important;
   }
 }
 </style>
