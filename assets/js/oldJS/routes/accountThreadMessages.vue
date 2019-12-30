eb87e64be858bb6e4db64c839903277d6cdf5e20
Andrey Matukhnov <dronic.x@gmail.com>
2019-12-30 14:23:22 +0200

    Fix live chat

--- assets/js/oldJS/routes/accountThreadMessages.vue
+++ assets/js/oldJS/routes/accountThreadMessages.vue
@@ -394,7 +394,7 @@
         const { reverseThreadMessages } = this;
         const lastConversationDivider = reverseThreadMessages.find(message => message.type && message.type.includes('conversation'))
 
-        return lastConversationDivider.type === 'conversation_end' && lastConversationDivider;
+        return lastConversationDivider && lastConversationDivider.type === 'conversation_end';
       },
 
       canRecheckCampaigns() {
