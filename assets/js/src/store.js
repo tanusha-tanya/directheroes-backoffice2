3d06e9333ba2359bf4da8be2cd2f7b173c9070f8
Andrey Matukhnov <dronic.x@gmail.com>
2020-02-26 22:27:43 +0200

    Increase update timeout to 2 sec.

--- assets/js/src/store.js
+++ assets/js/src/store.js
@@ -22,7 +22,7 @@ export default new Vuex.Store({
     loading: false,
     existConnection: null,
     globalError: false,
-    saveTimeout: 1000,
+    saveTimeout: 2000,
     onSaveHandler: null,
   },
   mutations: {
