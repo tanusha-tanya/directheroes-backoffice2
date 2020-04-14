850cbe59d4668556709246bfe7be3e97f812ac85
Andrey Matukhnov <dronic.x@gmail.com>
2020-04-14 06:54:55 +0200

    Add sign steps

---
+++ assets/js/src/registration/registration.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="dh-sign-app">
+    <div class="dh-sign-step">
+      <component
+        :is="wizardState"
+      ></component>
+    </div>
+  </div>
+</template>
+
+<script>
+import wizardMixin from '../mixins/wizard/wizard'
+import signUp from './wizard/sign-up'
+
+export default {
+  mixins: [ wizardMixin ],
+
+  components: {
+    signUp
+  },
+
+  created() {
+    const { setWizardState } = this;
+
+    setWizardState(null, 'signUp')
+  },
+}
+</script>
+
+<style lang="scss">
+body {
+  margin: 0;
+  height: 100vh;
+  background-color: $mainBGColor;
+  font: normal 14px Rubik, sans-serif;
+  color: $mainTextColor;
+}
+
+.dh-sign-app {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .dh-sign-step {
+    background: #FFFFFF;
+    box-shadow: 0px 2px 80px rgba(27, 30, 36, 0.05);
+    border-radius: 4px;
+    padding-top: 40px;
+  }
+
+  .dh-sign-title {
+    font-size: 30px;
+    line-height: 36px;
+    color: #000000;
+  }
+}
+</style>
---
+++ assets/js/src/registration/wizard/sign-up.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="dh-sign-up-form">
+    <div class="dh-sign-title">
+       Create new account
+    </div>
+  </div>
+</template>
+
+<script>
+import wizardStep from '../../mixins/wizard/wizard-step'
+
+export default {
+
+}
+</script>
+
+<style lang="scss">
+.dh-sign-up-form {
+
+}
+</style>
