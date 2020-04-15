3483e1a9991902fae71ceac47bc0719767057818
Andrey Matukhnov <dronic.x@gmail.com>
2020-04-15 15:05:00 +0200

    fix thanks email

--- assets/js/src/registration/wizard/thanks.vue
+++ assets/js/src/registration/wizard/thanks.vue
@@ -9,7 +9,7 @@
     </div>
     <div class="dh-sign-text dh-sign-text--small">
       You can contact us to get more info at <br/>
-      <a href="mailto:email@email.com">email@email.com</a>
+      <a :href="`mailto:${ contactEmail }`" target="_blank">{{contactEmail}}</a>
     </div>
     <dh-button @click="goToDH">Start now!</dh-button>
   </div>
@@ -21,6 +21,14 @@ import logoPic from '../../assets/logo-pic.svg'
 export default {
   components: {
     logoPic
+  },
+
+  computed: {
+    contactEmail() {
+      const { contactEmail } = dh;
+
+      return contactEmail;
+    }
   },
 
   methods: {
